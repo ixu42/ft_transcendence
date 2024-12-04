2609fc7abd349c3b11e9b4a0581903bc3d855920
Ivy Xu <xyan21418@gmail.com>
2024-12-04 16:23:29 +0200

    added a default avatar for users

--- srcs/backend/users/models.py
+++ srcs/backend/users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     display_name = models.CharField(max_length=50, unique=True, blank=True)
+    profile_picture = models.ImageField(upload_to='avatars/', blank=True, null=True, default='avatars/default.png')
 
     def __str__(self):
         return f"{{'username': '{self.username}', 'display_name': '{self.display_name}'}}"
