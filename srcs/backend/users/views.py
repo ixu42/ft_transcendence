a887e1e151ded03f728974e4b340eb7dfd31224a
Ivy Xu <xyan21418@gmail.com>
2024-12-04 11:48:56 +0200

    added view for processing user registration requests

--- srcs/backend/users/views.py
+++ srcs/backend/users/views.py
@@ -1,7 +1,26 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+import json
 
-# Create your views here.
+# remove this exemption later
+# on the frontend, include the X-CSRFToken header ('X-CSRFToken': csrftoken) in the POST request, retrieving the token from the cookie
+@csrf_exempt
+@require_POST # restricts to only accept POST requests
 def register_user(request):
-    return HttpResponse("Register a user")
-    # return render(request, "users/register.html")
+    try:
+        data = json.loads(request.body)
+        print(data)
+    except json.JSONDecodeError:
+        return JsonResponse({"errors": "Invalid JSON input"}, status=400)
+
+    form = UserCreationForm(data)
+    if form.is_valid():
+        try:
+            user = form.save()
+            return JsonResponse({"message": "User created"}, status=201)
+        except Exception as e:
+            return JsonResponse({"errors": str(e)}, status=500)
+    else:
+        return JsonResponse({'errors': form.errors}, status=400)
