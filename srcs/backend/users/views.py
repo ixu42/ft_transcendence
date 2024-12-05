c1a64e00cf66384b65a595c31aa2464e04eb3b26
Ivy Xu <xyan21418@gmail.com>
2024-12-05 15:48:06 +0200

    implemented basic user login functionality

--- srcs/backend/users/views.py
+++ srcs/backend/users/views.py
@@ -3,6 +3,8 @@ from django.views.decorators.http import require_POST
 import json
 from django.http import JsonResponse
 from users.forms import CustomUserCreationForm
+from django.contrib.auth import authenticate, login
+from users.models import CustomUser
 
 # remove this exemption later
 # on the frontend, include the X-CSRFToken header ('X-CSRFToken': csrftoken) in the POST request, retrieving the token from the cookie
@@ -19,14 +21,38 @@ def register_user(request):
         print(data)
         print(display_name)
     except json.JSONDecodeError:
-        return JsonResponse({"errors": "Invalid JSON input"}, status=400)
+        return JsonResponse({'errors': 'Invalid JSON input'}, status=400)
 
     form = CustomUserCreationForm(data)
     if form.is_valid():
         try:
             form.save()
-            return JsonResponse({"message": "User created"}, status=201)
+            return JsonResponse({'message': 'User created'}, status=201)
         except Exception as e:
-            return JsonResponse({"errors": str(e)}, status=500)
+            return JsonResponse({'errors': str(e)}, status=500)
     else:
         return JsonResponse({'errors': form.errors}, status=400)
+
+@csrf_exempt
+@require_POST
+def login_user(request):
+    if request.method == 'POST':
+        body = json.loads(request.body)
+        username = body.get('username')
+        password = body.get('password')
+
+        if not username or not password:
+            return JsonResponse({'errors': 'Username and password are required.'}, status=400)
+
+        try:
+            user = CustomUser.objects.get(username=username)
+        except CustomUser.DoesNotExist:
+            return JsonResponse({'errors': 'Username does not exist'}, status=401)
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return JsonResponse({'message': 'Login successful'}, status=200)
+        else:
+            return JsonResponse({'errors': 'Invalid password'}, status=401)
