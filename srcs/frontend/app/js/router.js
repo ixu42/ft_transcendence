b8355251b5b734f3ad36864a02163c978cb84928
alex <alexxpyykonen@gmail.com>
2025-01-14 18:50:23 +0200

    hide navbar and footer in the login and register

--- srcs/frontend/app/js/router.js
+++ srcs/frontend/app/js/router.js
@@ -72,6 +72,13 @@ const handleLocation = async () => {
   const path = window.location.hash || "#";
   const route = routes[path] || routes[404];
 
+  const navbar = document.getElementById("navbar-container");
+  const footer = document.getElementById("footer-container");
+  const hideNavbarAndFooter = ["#login", "#register"].includes(path);
+
+  if (navbar) navbar.style.display = hideNavbarAndFooter ? "none" : "block";
+  if (footer) footer.style.display = hideNavbarAndFooter ? "none" : "block";
+
   if (protectedRoutes.includes(path) && !isUserLoggedIn()) {
     redirectToLogin();
     return;
