81b3ec28b0baf45dc08be4de8b31d8b308630f4d
Ivy Xu <xyan21418@gmail.com>
2025-02-06 17:25:49 +0200

    for user login checked if user is already logged in

--- srcs/backend/users/views.py
+++ srcs/backend/users/views.py
@@ -12,47 +12,51 @@ from users.models import CustomUser
 def register_user(request):
     try:
         data = json.loads(request.body)
-
-        form = CustomUserCreationForm(data)
-        if form.is_valid():
-            try:
-                form.save()
-                return JsonResponse({"message": "User created."}, status=201)
-            except Exception as e:
-                return JsonResponse({"errors": str(e)}, status=500)
-        else:
-            return JsonResponse({"errors": form.errors}, status=400)
     except json.JSONDecodeError:
         return JsonResponse({"errors": "Invalid JSON input."}, status=400)
+
+    form = CustomUserCreationForm(data)
+    if form.is_valid():
+        try:
+            form.save()
+            return JsonResponse({"message": "User created."}, status=201)
+        except Exception as e:
+            return JsonResponse({"errors": str(e)}, status=500)
+    else:
+        return JsonResponse({"errors": form.errors}, status=400)
 
 
 @csrf_exempt
 @require_POST
 def login_user(request):
+    if request.user.is_authenticated:
+        return JsonResponse({"errors": "User is already authenticated."}, status=400)
+
     try:
         body = json.loads(request.body)
-        username = body.get("username")
-        password = body.get("password")
-
-        if not username or not password:
-            return JsonResponse(
-                {"errors": "Username and password are required."}, status=400
-            )
-
-        try:
-            user = CustomUser.objects.get(username=username)
-        except CustomUser.DoesNotExist:
-            return JsonResponse({"errors": "Username does not exist."}, status=401)
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return JsonResponse({"message": "Login successful."}, status=200)
-        else:
-            return JsonResponse({"errors": "Invalid password."}, status=401)
     except json.JSONDecodeError:
         return JsonResponse({"errors": "Invalid JSON input."}, status=400)
+    
+    username = body.get("username")
+    password = body.get("password")
+
+    if not username or not password:
+        return JsonResponse(
+            {"errors": "Username and password are required."}, status=400
+        )
+
+    try:
+        user = CustomUser.objects.get(username=username)
+    except CustomUser.DoesNotExist:
+        return JsonResponse({"errors": "Username does not exist."}, status=401)
+
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return JsonResponse({"message": "Login successful."}, status=200)
+    else:
+        return JsonResponse({"errors": "Invalid password."}, status=401)
 
 
 @csrf_exempt
