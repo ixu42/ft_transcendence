e610f3006c65abcaf7a40fe22781ed105eed0a21
Ivy Xu <xyan21418@gmail.com>
2025-02-06 17:20:21 +0200

    added users/logout/ endpoint

--- srcs/backend/users/urls.py
+++ srcs/backend/users/urls.py
@@ -6,4 +6,5 @@ app_name = "users"
 urlpatterns = [
     path("register/", views.register_user, name="register_user"),
     path("login/", views.login_user, name="login_user"),
+    path("logout/", views.logout_user, name="logout_user"),
 ]
--- srcs/backend/users/views.py
+++ srcs/backend/users/views.py
@@ -2,7 +2,7 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from users.forms import CustomUserCreationForm
 from users.models import CustomUser
 
@@ -12,41 +12,54 @@ from users.models import CustomUser
 def register_user(request):
     try:
         data = json.loads(request.body)
-    except json.JSONDecodeError:
-        return JsonResponse({"errors": "Invalid JSON input"}, status=400)
 
-    form = CustomUserCreationForm(data)
-    if form.is_valid():
-        try:
-            form.save()
-            return JsonResponse({"message": "User created"}, status=201)
-        except Exception as e:
-            return JsonResponse({"errors": str(e)}, status=500)
-    else:
-        return JsonResponse({"errors": form.errors}, status=400)
+        form = CustomUserCreationForm(data)
+        if form.is_valid():
+            try:
+                form.save()
+                return JsonResponse({"message": "User created."}, status=201)
+            except Exception as e:
+                return JsonResponse({"errors": str(e)}, status=500)
+        else:
+            return JsonResponse({"errors": form.errors}, status=400)
+    except json.JSONDecodeError:
+        return JsonResponse({"errors": "Invalid JSON input."}, status=400)
 
 
 @csrf_exempt
 @require_POST
 def login_user(request):
-    body = json.loads(request.body)
-    username = body.get("username")
-    password = body.get("password")
-
-    if not username or not password:
-        return JsonResponse(
-            {"errors": "Username and password are required."}, status=400
-        )
-
     try:
-        user = CustomUser.objects.get(username=username)
-    except CustomUser.DoesNotExist:
-        return JsonResponse({"errors": "Username does not exist"}, status=401)
+        body = json.loads(request.body)
+        username = body.get("username")
+        password = body.get("password")
 
-    user = authenticate(request, username=username, password=password)
+        if not username or not password:
+            return JsonResponse(
+                {"errors": "Username and password are required."}, status=400
+            )
 
-    if user is not None:
-        login(request, user)
-        return JsonResponse({"message": "Login successful"}, status=200)
+        try:
+            user = CustomUser.objects.get(username=username)
+        except CustomUser.DoesNotExist:
+            return JsonResponse({"errors": "Username does not exist."}, status=401)
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return JsonResponse({"message": "Login successful."}, status=200)
+        else:
+            return JsonResponse({"errors": "Invalid password."}, status=401)
+    except json.JSONDecodeError:
+        return JsonResponse({"errors": "Invalid JSON input."}, status=400)
+
+
+@csrf_exempt
+@require_POST
+def logout_user(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return JsonResponse({"message": "Logout successful."}, status=200)
     else:
-        return JsonResponse({"errors": "Invalid password"}, status=401)
+        return JsonResponse({"errors": "User is not authenticated."}, status=401)
