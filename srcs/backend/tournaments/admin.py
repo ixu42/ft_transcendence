cc77d0a2eef7877b186c8abb40b7accc38273efa
Ivy Xu <xyan21418@gmail.com>
2025-02-20 12:11:01 +0200

    configured django admin to display tournaments info

--- srcs/backend/tournaments/admin.py
+++ srcs/backend/tournaments/admin.py
@@ -1,1 +1,23 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Tournament
+
+
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "get_creator", "get_players", "started_at", "status")
+    list_filter = ("started_at", "status")
+    search_fields = ("name",)
+
+    def get_creator(self, obj):
+        return obj.creator.username
+
+    def get_players(self, obj):
+        return ", ".join(player.username for player in obj.players.all())
+
+    get_creator.short_description = "Creator"
+    get_players.short_description = "Players"
+
+    def has_add_permission(self, request):
+        return False
+
+
+admin.site.register(Tournament, TournamentAdmin)
