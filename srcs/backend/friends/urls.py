30d91299dbfccc549d33ef60110708bb34c48ed1
Ivy Xu <xyan21418@gmail.com>
2025-02-27 15:35:08 +0200

    added GET users/<user_id>/friends/requests/ for listing pending friend requests received by the logged-in user

--- srcs/backend/friends/urls.py
+++ srcs/backend/friends/urls.py
@@ -4,7 +4,12 @@ from . import views
 app_name = "friends"
 
 urlpatterns = [
+    # list friend (GET) / send a friend request (POST)
+    path("", views.friend_list_create, name="friend_list_create"),
+    # List pending friend requests for a user
     path(
-        "", views.friend_list_create, name="friend_list_create"
-    ),  # list friend (GET) / send a friend request (POST)
+        "requests/",
+        views.list_friend_requests,
+        name="list_friend_requests",
+    ),
 ]
--- srcs/backend/friends/views.py
+++ srcs/backend/friends/views.py
@@ -1,10 +1,11 @@
 from django.http import JsonResponse
-from django.views.decorators.http import require_POST, require_http_methods
+from django.views.decorators.http import require_GET, require_POST, require_http_methods
 from users.views import login_required_json
 from friends.models import FriendRequest
 from users.models import CustomUser
 
 
+# Send a friend request to the user with id specified in the url
 @login_required_json
 @require_POST
 def send_friend_request(request, user_id):
@@ -36,8 +37,7 @@ def send_friend_request(request, user_id):
 @require_http_methods(["GET", "POST"])
 def friend_list_create(request, user_id):
     if request.method == "GET":
-        user = request.user
-        if user.id != user_id:
+        if request.user.id != user_id:
             return JsonResponse(
                 {"errors": "You do not have permission to view friends of this user."},
                 status=403,
@@ -46,3 +46,23 @@ def friend_list_create(request, user_id):
         return JsonResponse({"friends": list(friends)})
     else:
         return send_friend_request(request, user_id)
+
+
+# List all the pending friend requests received by the logged-in user
+@login_required_json
+@require_GET
+def list_friend_requests(request, user_id):
+    user = request.user
+
+    if user.id != user_id:
+        return JsonResponse(
+            {"errors": "You cannot view other users' friend requests."}, status=403
+        )
+
+    requests = FriendRequest.objects.filter(receiver=user, status="pending")
+    request_list = [
+        {"id": req.id, "sender": req.sender.username, "sent at": req.created_at}
+        for req in requests
+    ]
+
+    return JsonResponse({"friend_requests": request_list}, safe=False)
