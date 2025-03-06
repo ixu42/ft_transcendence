4e22ef5bee1a647ab12b662dec4bafbd72d4d909
Bobbie Syväsalmi <bobbieapitzsch@gmail.com>
2025-03-06 10:33:15 +0200

    Updated AI, added selection for AI level and winning score

--- srcs/frontend/app/js/game/1v1.js
+++ srcs/frontend/app/js/game/1v1.js
@@ -4,15 +4,18 @@ const createGame = (isAI = false) => {
     const player2 = createPaddle(canvas.width - 10, canvas.height / 2 - 50);
     const ball = createBall(canvas.width / 2, canvas.height / 2);
     const context = canvas.getContext('2d');
-    const pause = false;
+    const state = isAI ? "levelSelection" : "scoreSelection";
+    const lastState = "prepare";
+    const aiLevel = 'none';
+    const winningScore = 0;
 
-    return { player, player2, ball, canvas, context, pause, isAI };
+    return { player, player2, ball, canvas, context, state, lastState, isAI, aiLevel, winningScore };
 };
 
 const updateGame = (game) => {
     movePaddle(game.player, game.canvas);
-    game.isAI ? moveAIPaddle(game.player2, game.ball, game.canvas) : movePaddle(game.player2, game.canvas);
-    !game.pause && moveBall(game.ball, game.player, game.player2, game.canvas);
+    game.isAI ? moveAIPaddle(game.player2, game.ball, game.canvas, game.aiLevel) : movePaddle(game.player2, game.canvas);
+    game.state === "game" && moveBall(game.ball, game.player, game.player2, game.canvas);
 };
 
 const drawScore = (game) => {
@@ -31,9 +34,44 @@ const drawGame = (game) => {
     game.context.fillStyle = '#fff';
     drawPaddle(game.context, game.player);
     drawPaddle(game.context, game.player2);
-    game.pause && drawPause(game);
+    game.state == "pause" && drawPause(game);
     drawBall(game.context, game.ball);
     drawScore(game);
+};
+
+const drawScoreSelection = (game) => {
+    const context = game.context;
+    context.clearRect(0, 0, game.canvas.width, game.canvas.height);
+    context.font = '30px Arial';
+    context.fillStyle = '#fff';
+    context.fillText('Select Winning Score', game.canvas.width / 2 - 100, game.canvas.height / 2 - 60);
+    context.fillText('1. 3 Points', game.canvas.width / 2 - 50, game.canvas.height / 2 - 20);
+    context.fillText('2. 7 Points', game.canvas.width / 2 - 50, game.canvas.height / 2 + 20);
+    context.fillText('3. Endless', game.canvas.width / 2 - 50, game.canvas.height / 2 + 60);
+    context.fillText('Press 1, 2, or 3 to select', game.canvas.width / 2 - 100, game.canvas.height / 2 + 100);
+};
+
+const drawGameOver = (game) => {
+    const context = game.context;
+    context.clearRect(0, 0, game.canvas.width, game.canvas.height);
+    context.font = '30px Arial';
+    context.fillStyle = '#fff';
+    context.fillText('Game Over', game.canvas.width / 2 - 50, game.canvas.height / 2 - 60);
+    if (game.isAI)
+    {
+        if (game.player.score >= game.winningScore)
+            context.fillText('You Win!', game.canvas.width / 2 - 50, game.canvas.height / 2 - 20);
+        else 
+            context.fillText('You lost!', game.canvas.width / 2 - 50, game.canvas.height / 2 - 20);
+    }
+    else
+    {
+        if (game.player.score >= game.winningScore)
+            context.fillText('Player 1 Wins!', game.canvas.width / 2 - 50, game.canvas.height / 2 - 20);
+        else 
+            context.fillText('Player 2 Wins!', game.canvas.width / 2 - 50, game.canvas.height / 2 - 20);
+    }
+    context.fillText('Press Space to Restart', game.canvas.width / 2 - 100, game.canvas.height / 2 + 20);
 };
 
 const resetGame = (game) => {
@@ -43,8 +81,21 @@ const resetGame = (game) => {
 };
 
 const gameLoop = (game) => {
-    updateGame(game);
-    drawGame(game);
+    if (game.state === "game" || game.state === "pause" || game.state === "prepare") {
+        updateGame(game);
+        drawGame(game);
+        if (game.winningScore > 0 && (game.player.score >= game.winningScore || game.player2.score >= game.winningScore))
+            game.state = "gameOver";
+    }
+    if (game.state === "levelSelection") {
+        drawLevelSelection(game);
+    }
+    if (game.state === "scoreSelection") {
+        drawScoreSelection(game);
+    }
+    if (game.state === "gameOver") {
+        drawGameOver(game);
+    }
     requestAnimationFrame(() => gameLoop(game));
 };
 
@@ -59,17 +110,4 @@ const initializeGame = () => {
         setupControls(game.player, game.player2, game);
         console.log("Starting game loop");
         gameLoop(game);
-};
-
-const initializeAIGame = () => {
-    const canvas = document.getElementById('pong');
-    if (!canvas) {
-        console.error("Canvas element '#pong' not found.");
-        resolve([0, 0]);
-        return;
-    }
-    const game = createGame(true);
-    setupControls(game.player, game.player2, game);
-    console.log("Starting game loop");
-    gameLoop(game);
 };
