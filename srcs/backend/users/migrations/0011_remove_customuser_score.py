dcd709564222cbc51df36179b1e45ead21e37978
Ivy Xu <xyan21418@gmail.com>
2025-03-12 11:32:00 +0200

    added tests for update_avatar view; refactored tests; reformatting

--- srcs/backend/users/migrations/0011_remove_customuser_score.py
+++ srcs/backend/users/migrations/0011_remove_customuser_score.py
@@ -4,14 +4,13 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('users', '0010_customuser_last_active'),
+        ("users", "0010_customuser_last_active"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='customuser',
-            name='score',
+            model_name="customuser",
+            name="score",
         ),
     ]
--- srcs/backend/users/tests.py
+++ srcs/backend/users/tests.py
@@ -1,9 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 import json
+from django.core.files.uploadedfile import SimpleUploadedFile
+from PIL import Image
+import io
 
 User = get_user_model()
+
+
+class BaseTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username="testuser", password="securepassword123"
+        )
+
+    def login(self):
+        self.client.force_login(self.user)
+
+    def tearDown(self):
+        self.client.logout()
 
 
 class TestRegisterUser(TestCase):
@@ -37,11 +54,11 @@ class TestRegisterUser(TestCase):
         self.assertEqual(response.json()["errors"], "Invalid JSON input.")
 
     def test_register_user_username_exists(self):
-        User.objects.create_user(username="testuser", password="password123")
+        User.objects.create_user(username="testuser", password="securepassword123")
         existing_data = {
             "username": "testuser",
-            "password1": "password123",
-            "password2": "password123",
+            "password1": "securepassword123",
+            "password2": "securepassword123",
         }
         response = self.make_request(json.dumps(existing_data))
 
@@ -56,8 +73,8 @@ class TestRegisterUser(TestCase):
     def test_register_user_password_mismatch(self):
         mismatch_data = {
             "username": "testuser",
-            "password1": "password123",
-            "password2": "password456",
+            "password1": "securepassword123",
+            "password2": "securepassword456",
         }
         response = self.make_request(json.dumps(mismatch_data))
 
@@ -79,13 +96,9 @@ class TestRegisterUser(TestCase):
         self.assertIsInstance(data["errors"], dict)
 
 
-class TestLoginUser(TestCase):
+class TestLoginUser(BaseTestCase):
     def setUp(self):
         self.url = reverse("users:login_user")
-
-        self.user = User.objects.create_user(
-            username="testuser", password="password123"
-        )
 
     def make_request(self, request_body):
         return self.client.post(
@@ -93,7 +106,7 @@ class TestLoginUser(TestCase):
         )
 
     def test_login_user_success(self):
-        valid_data = {"username": "testuser", "password": "password123"}
+        valid_data = {"username": "testuser", "password": "securepassword123"}
         response = self.make_request(json.dumps(valid_data))
 
         self.assertEqual(response.status_code, 200)
@@ -104,8 +117,8 @@ class TestLoginUser(TestCase):
         self.assertEqual(data["message"], "Login successful.")
 
     def test_login_user_already_logged_in(self):
-        self.client.force_login(self.user)
-        valid_data = {"username": "testuser", "password": "password123"}
+        self.login()
+        valid_data = {"username": "testuser", "password": "securepassword123"}
         response = self.make_request(json.dumps(valid_data))
 
         self.assertEqual(response.status_code, 400)
@@ -114,7 +127,7 @@ class TestLoginUser(TestCase):
         self.assertEqual(data["errors"], "User is already logged in.")
 
     def test_login_user_username_missing(self):
-        missing_username = {"password": "password123"}
+        missing_username = {"password": "securepassword123"}
         response = self.make_request(json.dumps(missing_username))
 
         self.assertEqual(response.status_code, 400)
@@ -123,19 +136,15 @@ class TestLoginUser(TestCase):
         self.assertEqual(data["errors"], "Username and password are required.")
 
 
-class TestLogoutUser(TestCase):
+class TestLogoutUser(BaseTestCase):
     def setUp(self):
         self.url = reverse("users:logout_user")
-
-        self.user = User.objects.create_user(
-            username="testuser", password="password123"
-        )
 
     def make_request(self):
         return self.client.post(self.url)
 
     def test_logout_user_success(self):
-        self.client.force_login(self.user)
+        self.login()
         response = self.make_request()
 
         self.assertEqual(response.status_code, 200)
@@ -154,16 +163,13 @@ class TestLogoutUser(TestCase):
         self.assertEqual(data["errors"], "User is not authenticated.")
 
 
-class TestUserProfile(TestCase):
+class TestUserProfile(BaseTestCase):
     def setUp(self):
-        self.user = User.objects.create_user(
-            username="testuser", password="password123"
-        )
         self.valid_url = reverse("users:user_profile", args=[self.user.id])
         self.invalid_url = reverse("users:user_profile", args=[self.user.id + 1])
+        self.login()
 
     def test_user_profile_not_found(self):
-        self.client.force_login(self.user)
         response = self.client.get(self.invalid_url)
 
         self.assertEqual(response.status_code, 403)
@@ -174,7 +180,6 @@ class TestUserProfile(TestCase):
         )
 
     def test_get_user_profile_success(self):
-        self.client.force_login(self.user)
         response = self.client.get(self.valid_url)
 
         self.assertEqual(response.status_code, 200)
@@ -189,7 +194,6 @@ class TestUserProfile(TestCase):
         self.assertIn("total_losses", data)
 
     def test_update_user_profile_success(self):
-        self.client.force_login(self.user)
         request_body = {
             "deactivate": False,
             "username": "user42",
@@ -210,8 +214,7 @@ class TestUserProfile(TestCase):
         self.assertEqual(self.user.username, "user42")
 
     def test_update_user_profile_errors(self):
-        User.objects.create_user(username="existinguser", password="password123")
-        self.client.force_login(self.user)
+        User.objects.create_user(username="existinguser", password="securepassword123")
         request_body = {
             "deactivate": False,
             "username": "existinguser",
@@ -232,7 +235,6 @@ class TestUserProfile(TestCase):
         )
 
     def test_deactivate_user_profile_success(self):
-        self.client.force_login(self.user)
         request_body = {"deactivate": True}
         response = self.client.patch(
             self.valid_url,
@@ -250,7 +252,6 @@ class TestUserProfile(TestCase):
         self.assertEqual(self.user.is_active, False)
 
     def test_delete_user_profile_success(self):
-        self.client.force_login(self.user)
         response = self.client.delete(self.valid_url)
 
         data = response.json()
@@ -261,12 +262,10 @@ class TestUserProfile(TestCase):
         self.assertEqual(User.objects.filter(id=self.user.id).exists(), False)
 
 
-class TestUpdatePassword(TestCase):
+class TestUpdatePassword(BaseTestCase):
     def setUp(self):
-        self.user = User.objects.create_user(
-            username="testuser", password="securepassword123"
-        )
         self.url = reverse("users:update_password", args=[self.user.id])
+        self.login()
 
     def make_request(self, request_body):
         return self.client.post(
@@ -274,7 +273,6 @@ class TestUpdatePassword(TestCase):
         )
 
     def test_update_password_success(self):
-        self.client.force_login(self.user)
         request_body = {
             "old_password": "securepassword123",
             "new_password1": "securepassword456",
@@ -290,7 +288,6 @@ class TestUpdatePassword(TestCase):
         self.assertEqual(data["message"], "User password updated.")
 
     def test_update_password_old_pass_invalid(self):
-        self.client.force_login(self.user)
         request_body = {
             "old_password": "invalidpassword123",
             "new_password1": "securepassword456",
@@ -305,7 +302,6 @@ class TestUpdatePassword(TestCase):
         self.assertEqual(data["errors"]["old_password"], ["Old password is incorrect."])
 
     def test_update_password_new_pass_same_as_old(self):
-        self.client.force_login(self.user)
         request_body = {
             "old_password": "securepassword123",
             "new_password1": "securepassword123",
@@ -323,7 +319,6 @@ class TestUpdatePassword(TestCase):
         )
 
     def test_update_password_new_pass_no_match(self):
-        self.client.force_login(self.user)
         request_body = {
             "old_password": "securepassword123",
             "new_password1": "securepassword456",
@@ -337,4 +332,95 @@ class TestUpdatePassword(TestCase):
         self.assertIn("new_password2", data["errors"])
         self.assertEqual(
             data["errors"]["new_password2"], ["The two password fields didn’t match."]
+        )
+
+
+@override_settings(
+    MEDIA_ROOT="/tmp/test_media"
+)  # Uploaded test files are removed on container removal
+class TestUpdateAvatar(BaseTestCase):
+    def setUp(self):
+        self.url = reverse("users:update_avatar")
+        self.login()
+
+    def make_request(self, request_body={}):
+        return self.client.post(self.url, data=request_body)
+
+    def generate_valid_test_image(self):
+        """Create a simple in-memory test image."""
+        image_io = io.BytesIO()
+        # Create a 100x100 blue image
+        image = Image.new("RGB", (100, 100), "blue")
+        image.save(image_io, format="JPEG")
+        # Reset the file pointer to the beginning of image stored in a BytesIO buffer
+        image_io.seek(0)
+        return SimpleUploadedFile(
+            "avatar.jpg", image_io.read(), content_type="image/jpeg"
+        )
+
+    def generate_test_image_invalid_ext(self):
+        image_io = io.BytesIO()
+        image = Image.new("RGB", (100, 100), "blue")
+        image.save(image_io, format="GIF")
+        image_io.seek(0)
+        return SimpleUploadedFile(
+            "avatar.gif", image_io.read(), content_type="image/gif"
+        )
+
+    def generate_test_image_invalid_size(self):
+        from PIL import Image
+
+        # Set a large limit for image pixels
+        Image.MAX_IMAGE_PIXELS = 1000000000
+
+        image_io = io.BytesIO()
+        # Approx. 3.35 MB | len(image_io.getvalue()) / (1024 * 1024) to check file size in MB
+        image = Image.new("RGB", (15000, 15000), "blue")
+        image.save(image_io, format="JPEG")
+        image_io.seek(0)
+
+        return SimpleUploadedFile(
+            "avatar.jpg", image_io.read(), content_type="image/jpeg"
+        )
+
+    def test_update_avatar_success(self):
+        image = self.generate_valid_test_image()
+        response = self.make_request(request_body={"avatar": image})
+
+        data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("avatar_url", data)
+
+    def test_update_avatar_no_file_uploaded(self):
+        response = self.make_request()
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("errors", response.json())
+        self.assertEqual(response.json()["errors"], "No file uploaded.")
+
+    def test_update_avatar_invalid_file_ext(self):
+        image = self.generate_test_image_invalid_ext()
+        response = self.make_request(request_body={"avatar": image})
+
+        self.assertEqual(response.status_code, 400)
+        data = response.json()
+        self.assertIn("errors", data)
+        self.assertIn("avatar", data["errors"])
+        self.assertEqual(
+            data["errors"]["avatar"],
+            [
+                "File extension “gif” is not allowed. Allowed extensions are: jpg, jpeg, png."
+            ],
+        )
+
+    def test_update_avatar_size_too_large(self):
+        image = self.generate_test_image_invalid_size()
+        response = self.make_request(request_body={"avatar": image})
+
+        self.assertEqual(response.status_code, 400)
+        data = response.json()
+        self.assertIn("errors", data)
+        self.assertIn("avatar", data["errors"])
+        self.assertEqual(
+            data["errors"]["avatar"],
+            ["File size exceeds the limit 3.0 MB."],
         )
