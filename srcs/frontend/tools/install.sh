96f702ae876308e47e04c536b522f2d12a360e14
Dmitrii Shatilov <shatilovdr@gmail.com>
2025-04-10 17:12:36 +0300

    Configure ModSecurity WAF

--- srcs/frontend/tools/install.sh
+++ srcs/frontend/tools/install.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-# Installing ModSecurity firewall 
+# Install ModSecurity firewall 
 tar -xzvf modsecurity-v3.0.14.tar.gz
 cd modsecurity-v3.0.14
 ./build.sh
@@ -9,22 +9,30 @@ make -j$(nproc)
 make install
 cd /
 
-# Downloading and installing ModSecurity-nginx connector
+
+# Download and install ModSecurity-nginx connector
 tar -xzvf nginx-1.27.4.tar.gz
-git clone https://github.com/owasp-modsecurity/ModSecurity-nginx
+git clone --depth 1 https://github.com/owasp-modsecurity/ModSecurity-nginx
 cd nginx-1.27.4
+CONF_ARGS=$(nginx -V 2>&1 | grep 'configure arguments:' | cut -d: -f2-)
+echo $CONF_ARGS | xargs ./configure --with-compat --add-dynamic-module=/ModSecurity-nginx
+make modules
+cp objs/ngx_http_modsecurity_module.so /etc/nginx/modules/
 
-# Get configure arguments for nginx
-input=$(nginx -V 2>&1 | grep 'configure arguments:' | cut -d: -f2-)
-IFS=$'\n' read -d '' -r -a CONF_ARGS < <(echo "$input" | sed 's/ --/\n--/g')
-for element in "${CONF_ARGS[@]}"; do
-    echo "$element"
-done
-./configure --with-compat --add-dynamic-module=/ModSecurity-nginx "${CONF_ARGS[@]}"
+# Configure Modsecurity
+mkdir /etc/nginx/modsecurity
+cd /etc/nginx/modsecurity
+cp /modsecurity-v3.0.14/modsecurity.conf-recommended modsecurity.conf
+cp /modsecurity-v3.0.14/unicode.mapping .
+git clone --depth 1 https://github.com/coreruleset/coreruleset.git
+echo "Include /etc/nginx/modsecurity/coreruleset/crs-setup.conf" >> modsecurity.conf
+echo "Include /etc/nginx/modsecurity/coreruleset/rules/*.conf" >> modsecurity.conf
 
 
-# # Remove all installation files 
-# rm -rf modsecurity-v3.0.14.tar.gz \
-#        modsecurity-nginx-v1.0.3.tar.gz \
-#        modsecurity-v3.0.14 
-      #  modsecurity-nginx-v1.0.3
+# Remove all installation files 
+rm -rf /modsecurity-v3.0.14.tar.gz \
+       /modsecurity-v3.0.14 \
+       /nginx-1.27.4.tar.gz \
+       /ModSecurity-nginx \
+       /nginx-1.27.4 \
+       /install.sh
