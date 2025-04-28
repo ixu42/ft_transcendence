c727665c8ea8aa980b012cd6c25088a15dd4b6a7
Ivy Xu <xyan21418@gmail.com>
2025-04-28 14:21:49 +0300

    1v1 game fix: validate path param for create_local_game; prevent both players from being the same user; update api docs

--- srcs/backend/games/views.py
+++ srcs/backend/games/views.py
@@ -1,16 +1,18 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods, require_POST
-from users.views import custom_login_required
 from django.contrib.auth import get_user_model
+from users.views import custom_login_required as custom_login_required_id_as_path_param
+from tournaments.views import (
+    custom_login_required as custom_login_required_id_as_query_param,
+)
 from .models import Game
 from .forms import GameStatsForm, LocalGameForm
-from tournaments.views import custom_login_required as custom_login_required_multi_users
 
 User = get_user_model()
 
 
-@custom_login_required
+@custom_login_required_id_as_path_param
 @require_POST
 def create_local_game_guest(request, user_id):
     user = User.objects.get(id=user_id)
@@ -21,7 +23,9 @@ def create_local_game_guest(request, user_id):
         {"message": "Local game created.", "game_id": game.id}, status=201
     )
 
-@custom_login_required_multi_users
+
+@custom_login_required_id_as_path_param
+@custom_login_required_id_as_query_param
 @require_POST
 def create_local_game(request, user_id):
     user1 = User.objects.get(id=user_id)
@@ -33,7 +37,8 @@ def create_local_game(request, user_id):
         {"message": "Local game created.", "game_id": game.id}, status=201
     )
 
-@custom_login_required
+
+@custom_login_required_id_as_path_param
 @require_POST
 def create_ai_game(request, user_id):
     user = User.objects.get(id=user_id)
@@ -42,7 +47,7 @@ def create_ai_game(request, user_id):
     return JsonResponse({"message": "AI game created.", "game_id": game.id}, status=201)
 
 
-@custom_login_required
+@custom_login_required_id_as_path_param
 @require_http_methods(["PATCH"])
 def save_game_stats(request, game_id, user_id):
     """
